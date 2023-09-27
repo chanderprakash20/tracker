67bf24953ccfe273fa7a77a34a5f7c60da5e509c
chanderprakash20 <chander132002@gmail.com>
2023-09-27 15:22:19 +0530

    My first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
+//INTERNAL IMPORT
+import { Footer, Banner, NavBar } from "../Components";
+import { TrackingProvider } from "../Conetxt/TrackingContext";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <TrackingProvider>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </TrackingProvider>
+    </>
+  );
 }
